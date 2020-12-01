807c33a708e6d65fdedea07d0ca433cb41d0f4bb
catwhitmer <catwhitmer@gmail.com>
2020-12-01 12:05:13 -0500

    created todo model and created new todo

---
+++ src/controllers/todos.ts
@@ -0,0 +1,12 @@
+import express, { RequestHandler } from "express";
+
+import { Todo } from "../models/todo";
+
+const TODOS: Todo[] = [];
+
+export const createTodo: RequestHandler = (req, res, next) => {
+  const text = (req.body as { text: string }).text;
+  const newTodo = new Todo(Math.random().toString(), text);
+
+  TODOS.push(newTodo);
+};
